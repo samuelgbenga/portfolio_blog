2b2dca7940443491cc5c69dd3d6273fe1b586591
samuelgbenga <samuelgbenga972@gmail.com>
2023-02-18 17:19:06 +0100

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import UserProfile from "./components/userProfile";
 import UserPortfolio from "./components/userPortfolio";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 const App = () => {
+  const [dark, setDark] = useState(true);
   return (
-    <div className="w-full lg:min-h-screen bg-dark-theme text-white">
-      <div className="w-full flex flex-col min-h-screen lg:flex-row lg:justify-between relative">
-        <div className="lg:sticky lg:top-0 lg:h-screen  p-20  grow">
+    <div
+      className={`w-full lg:min-h-screen bg-white-theme text-black ${
+        dark && "dark"
+      } relative`}
+    >
+      <div className="w-full flex flex-col min-h-screen lg:flex-row lg:justify-between relative dark:text-white dark:bg-dark-theme">
+        <div className="lg:sticky lg:top-0 lg:h-screen  p-20  grow ">
           <UserProfile />
         </div>
         <div className="grow ">
           <UserPortfolio />
         </div>
+      </div>
+      <div
+        className="fixed top-[92%] right-10  cursor text-[40px]"
+        onClick={() => setDark((prev) => !prev)}
+      >
+        {dark ? (
+          <BsFillSunFill className="text-white" />
+        ) : (
+          <BsFillMoonStarsFill className="" />
+        )}
       </div>
     </div>
   );
